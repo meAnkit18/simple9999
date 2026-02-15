6190801f1ad89f8059300ebc8c5b897dcb04139e
Ankit Kumar <ankit1842kumar@gmail.com>
2026-02-15 12:39:20 +0530

    trackpage fix

--- components/TrackPage.tsx
+++ components/TrackPage.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID!
@@ -9,16 +9,24 @@ export default function TrackPage() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
+  const [mounted, setMounted] = useState(false)
+
+  // wait until client hydration
   useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  useEffect(() => {
+    if (!mounted) return
     if (!window.gtag) return
 
-    const url =
-      pathname + (searchParams?.toString() ? `?${searchParams}` : '')
+    const query = searchParams?.toString()
+    const url = query ? `${pathname}?${query}` : pathname
 
     window.gtag('config', GA_ID, {
       page_path: url,
     })
-  }, [pathname, searchParams])
+  }, [mounted, pathname, searchParams])
 
   return null
 }
